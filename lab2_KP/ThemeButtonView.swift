f6097ba535504c760eb18759ca1fe6699115bd39
klaudiaparczynska <klaudia.parczynska@pollub.edu.pl>
2024-04-27 19:10:25 +0200

    lab2 zad 2

---
+++ lab2_KP/ThemeButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeButtonView.swift
+//  lab2_KP
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+struct ThemeButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeButtonView()
+}
