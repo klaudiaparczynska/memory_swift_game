75115d3d2e9c33f0bff41cf79943edca1810a040
a <a>
2024-04-27 17:03:50 +0200

    Initial Commit

---
+++ lab2_KP/lab2_KPApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2_KPApp.swift
+//  lab2_KP
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2_KPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
